9fa02ddf5a492deaf97865961e83bc6f92403d88
Fronikuniu <Fronikuniu@gmail.com>
2021-06-03 15:26:21 +0200

    Added class component from the HeroesFeatured component

--- src/Components/HeroesFeatured/HeroesFeatured.js
+++ src/Components/HeroesFeatured/HeroesFeatured.js
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getBasicHeroInfoById } from '../../Requests/requests';
 import HeroFeatured from './HeroFeatured/HeroFeatured';
 import Loader from '../Loader/Loader';
 import './HeroesFeatured.css';
 
-export default function HeroesFeatured() {
+function HeroesFeatured() {
   const [featuredHeroesList, setFeaturedHeroesList] = useState([]);
   let [featuredHeroesIds, setFeaturedHeroesIds] = useState([1, 2, 3, 4]);
   const [isLoading, setLoadingState] = useState(true);
@@ -38,8 +38,6 @@ export default function HeroesFeatured() {
     setLoadingState(false);
   };
 
-  console.log(featuredHeroesIds);
-
   return (
     <>
       <section className="featured">
@@ -71,3 +69,84 @@ export default function HeroesFeatured() {
     </>
   );
 }
+export default HeroesFeatured;
+
+// SAME COMPONENT AS CLASS COMPONENT
+
+// class HeroesFeatured extends React.Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       featuredHeroesList: [],
+//       isLoading: true,
+//       featuredHeroesIds: [1, 2, 3, 4],
+//     };
+//   }
+
+//   componentDidMount() {
+//     this.fetchAndDisplayFeaturedHeroes();
+//   }
+
+//   generateRandomIds = () => {
+//     this.setState({ isLoading: true });
+
+//     let numberOfId;
+//     this.setState({ featuredHeroesIds: (this.state.featuredHeroesIds = []) });
+
+//     for (let i = 0; i < 4; i++) {
+//       numberOfId = Math.floor(Math.random() * (731 - 0 + 1)) + 0; // Math.floor(Math.random() * (Max - Min + 1)) + Min
+//       this.setState({ featuredHeroesIds: this.state.featuredHeroesIds.push(numberOfId) });
+//     }
+
+//     this.setState({ featuredHeroesIds: numberOfId });
+//     this.fetchAndDisplayFeaturedHeroes();
+//   };
+
+//   fetchAndDisplayFeaturedHeroes = async () => {
+//     let heroes = [];
+//     for (const heroId of this.state.featuredHeroesIds) {
+//       const data = await getBasicHeroInfoById(heroId); // {data} tell us, we only want to retrieve only object data from object || {data} mówi nam, ze chcemy pobrac tylko obiekt data z obiektu
+//       heroes.push(data);
+//     }
+
+//     this.setState({ featuredHeroesList: heroes, isLoading: false });
+//   };
+
+//   render() {
+//     const { isLoading, featuredHeroesList } = this.state;
+
+//     return (
+//       <>
+//         <section className="featured">
+//           <h1>Featured Heroes</h1>
+//           {!isLoading && (
+//             <div className="featured__list">
+//               {featuredHeroesList.map(({ name, imgUrl, powerstats, id }) => (
+//                 <HeroFeatured key={id} name={name} powerstats={powerstats} imgUrl={imgUrl} id={id} />
+//               ))}
+//             </div>
+//           )}{' '}
+//           {isLoading && (
+//             <div className="loader-container">
+//               <Loader />
+//             </div>
+//           )}
+//         </section>
+
+//         <section className="featured__hero__refresh">
+//           {!isLoading && (
+//             <div className="container">
+//               <button onClick={() => this.generateRandomIds()} className="featured__button">
+//                 Show new Featured Heroes
+//               </button>
+//             </div>
+//           )}
+//           {isLoading && <></>}{' '}
+//         </section>
+//       </>
+//     );
+//   }
+// }
+
+// export default HeroesFeatured;
