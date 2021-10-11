e589c804cb07def9e1cf8d8c630499498a816188
Fronikuniu <Fronikuniu@gmail.com>
2021-10-11 19:52:30 +0200

    Try to fix error with deploy on netlify

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,8 +1,8 @@
-import combatIcon from './combaticon.svg';
-import durabilityIcon from './durabilityicon.svg';
-import intelligenceIcon from './intelligenceicon.svg';
-import powerIcon from './powericon.svg';
-import speedIcon from './speedicon.svg';
-import strengthIcon from './strengthicon.svg';
+import combatIcon from '../../assets/icons/combaticon.svg';
+import durabilityIcon from '../../assets/icons/durabilityicon.svg';
+import intelligenceIcon from '../../assets/icons/intelligenceicon.svg';
+import powerIcon from '../../assets/icons/powericon.svg';
+import speedIcon from '../../assets/icons/speedicon.svg';
+import strengthIcon from '../../assets/icons/strengthicon.svg';
 
 export { combatIcon, durabilityIcon, intelligenceIcon, powerIcon, speedIcon, strengthIcon };
