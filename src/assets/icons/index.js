820c9472df295994ca7b37d72de08f444f3bfd4d
Fronikuniu <Fronikuniu@gmail.com>
2021-10-11 20:12:07 +0200

    fix deploy

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,8 +1,8 @@
-import combatIcon from '../../assets/icons/combaticon.svg';
-import durabilityIcon from '../../assets/icons/durabilityicon.svg';
-import intelligenceIcon from '../../assets/icons/intelligenceicon.svg';
-import powerIcon from '../../assets/icons/powericon.svg';
-import speedIcon from '../../assets/icons/speedicon.svg';
-import strengthIcon from '../../assets/icons/strengthicon.svg';
+import combatIcon from './src/assets/icons/combaticon.svg';
+import durabilityIcon from './src/assets/icons/durabilityicon.svg';
+import intelligenceIcon from './src/assets/icons/intelligenceicon.svg';
+import powerIcon from './src/assets/icons/powericon.svg';
+import speedIcon from './src/assets/icons/speedicon.svg';
+import strengthIcon from './src/assets/icons/strengthicon.svg';
 
 export { combatIcon, durabilityIcon, intelligenceIcon, powerIcon, speedIcon, strengthIcon };
