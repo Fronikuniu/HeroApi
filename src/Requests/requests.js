98e4264f5b400d5f0d96ef77c5c6ddcdeb82f504
Fronikuniu <Fronikuniu@gmail.com>
2021-10-15 17:15:04 +0200

    Add netlify proxy

--- src/Requests/requests.js
+++ src/Requests/requests.js
@@ -1,15 +1,17 @@
 import axios from 'axios';
 
+const PROXY = window.location.hostname === 'localhost' ? 'https://cors-anywhere.herokuapp.com' : '/cors-proxy';
+
 export const getBasicHeroInfoById = async (id) => {
-  const { data: powerstats } = await axios.get(`/api/${process.env.REACT_APP_HEROAPI_ACCESS_TOKEN}/${id}/powerstats`);
-  const { data: image } = await axios.get(`/api/${process.env.REACT_APP_HEROAPI_ACCESS_TOKEN}/${id}/image`);
+  const { data: powerstats } = await axios.get(`${PROXY}/https://superheroapi.com/api/${process.env.REACT_APP_HEROAPI_ACCESS_TOKEN}/${id}/powerstats`);
+  const { data: image } = await axios.get(`${PROXY}/https://superheroapi.com/api/${process.env.REACT_APP_HEROAPI_ACCESS_TOKEN}/${id}/image`);
   return { name: powerstats.name, powerstats, imgUrl: image.url, id: powerstats.id };
 };
 
 export const getFullHeroInfoById = (id) => {
-  return axios.get(`/api/${process.env.REACT_APP_HEROAPI_ACCESS_TOKEN}/${id}`);
+  return axios.get(`${PROXY}/https://superheroapi.com/api/${process.env.REACT_APP_HEROAPI_ACCESS_TOKEN}/${id}`);
 };
 
 export const searchHeroByName = (name) => {
-  return axios.get(`/api/${process.env.REACT_APP_HEROAPI_ACCESS_TOKEN}/search/${name}`);
+  return axios.get(`${PROXY}/https://superheroapi.com/api/${process.env.REACT_APP_HEROAPI_ACCESS_TOKEN}/search/${name}`);
 };
